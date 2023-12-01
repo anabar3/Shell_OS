334e12ef130ae066acd6ca5db87854d2ca250dda
anabar3 <ana.barrera@udc.es>
2023-12-01 09:03:20 +0100

    list4

---
+++ shellfunc3.c
@@ -0,0 +1,6 @@
+//AUTHORS - Group 6.1 - 14/12/2023
+//Name: Javier Carballal Morgade    Login: javier.carballal.morgade@udc.es
+//Name: Ana Barrera Novas           Login: ana.barrera@udc.es
+
+#include "shellfunc.h"
+
